5b9c907c33b05cda48cb69538292a5ff9e0c8380
Takfarinas <takfagaming@gmail.com>
2025-10-07 01:30:10 -0400

    menu fini

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,16 +4,58 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public string gameSceneName = "SampleScene";
+    public AudioSource ButtonSound;
+    public AudioSource MenuMusic;
+
+    void Start()
+    {
+        if (MenuMusic != null && !MenuMusic.isPlaying)
+        {
+            MenuMusic.loop = true;
+            MenuMusic.Play();
+        }
+    }
 
     public void PlayGame()
+    {
+        if (ButtonSound != null)
+        {
+            ButtonSound.Play();
+            Invoke("LoadGameScene", ButtonSound.clip.length);
+        }
+        else
+        {
+            LoadGameScene();
+        }
+    }
+
+    void LoadGameScene()
     {
         SceneManager.LoadSceneAsync(gameSceneName);
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        if (ButtonSound != null)
+        {
+            ButtonSound.Play();
+            Invoke("QuitAfterSound", ButtonSound.clip.length);
+        }
+        else
+        {
+            QuitAfterSound();
+        }
+    }
+
+    void QuitAfterSound()
+    {
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
     }
 }
-
-
